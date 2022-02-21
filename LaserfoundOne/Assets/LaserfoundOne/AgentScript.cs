b726b6cef84bb59f16fe4f41446f5e2f3c34fb6c
yachty66 <68147541+yachty66@users.noreply.github.com>
2022-02-21 13:19:11 +0000

    W

--- LaserfoundOne/Assets/LaserfoundOne/AgentScript.cs
+++ LaserfoundOne/Assets/LaserfoundOne/AgentScript.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Threading;
+using System.Timers;
+using System.Threading.Tasks;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
 using Unity.MLAgents.Sensors;
 
+
+
 public class AgentScript : Agent{
+    //Notes
+    /*
+    bevor ich endepisode mache kann ich ja einen timer einbauen
+
+    problem ist dass ich addforce nicht sehe weil direkt endepisode eintrifft nach dem treffen vom laser
+    ich könnten einen timer erstellen
+
+
+    
+    */
+    //variable for    
+    public bool checkLoopEntryForEndEpisode = false;
+    public bool checkIfRayMovedGoal = false;
+    public int countForEndEpisode = 0;
 
     public bool checkIfRayHitsObject = false;
     
     public override void OnEpisodeBegin()
     {
-        transform.position = Vector3.zero;
+        Debug.Log("Kevin");
+        this.transform.localPosition = new Vector3(0f, 0f, 0f);
+        Debug.Log("Kevin2");
     }
 
     //helper method to see if goal gets hit from RayPerceptionSensor
@@ -39,6 +61,7 @@ public class AgentScript : Agent{
                     //bin davon ausgegangen, dass das immer nur dann passiert wenn ray auch wirklich trifft
                     //set bool true if object gets hit
                     checkIfRayHitsObject = true;
+                    AddReward(1.0f);
                 }
                 //set bool for ray doesn't hit goal
                 else
@@ -51,17 +74,79 @@ public class AgentScript : Agent{
 
     //Agent observations which come with every (frame/updadte)
     public override void CollectObservations(VectorSensor sensor){
+        sensor.AddObservation(transform.localPosition);
         RayCastInfo(GetComponent<RayPerceptionSensorComponent3D>());
+        //EndEpisode gets in CollectObservations called recursively
+        //EndEpisode();
     }
 
     //gets discrete or continous values for actions
     public override void OnActionReceived(ActionBuffers actions){
         float moveX = actions.ContinuousActions[0];
         float moveZ = actions.ContinuousActions[1];
-        float moveSpeed = 10f;
-        transform.position += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+        float moveSpeed = 3f;
+        transform.localPosition += new Vector3(moveX, 0, moveZ) * Time.deltaTime * moveSpeed;
+
+
+
+        //i could accumulate reward and than 
+
+       /*if(Convert.ToInt32((DateTime.Now.TimeOfDay - date).TotalSeconds) > 2){
+            Debug.Log("Tst");
+        }*/
+        
+
+        //abfrage ob timer eine certain zeit erreicht hat wenn ja dann endepisode (2sek)
+
+        //wenn der Ray getroffen hat dann kommt es auch automatisch zu einem Treffer vom Laser und daher zum reward. nach jedem reward beginnt die Epsiode neu.
+
+        //ich glaube weil die methode x mal aufgerufen wird...
+        countForEndEpisode =+ 1;
+        //Debug.Log(countForEndEpisode);
+        if(checkIfRayMovedGoal == true /*&& countForEndEpisode == 1 && checkLoopEntryForEndEpisode == false*/){
+            checkLoopEntryForEndEpisode = true;
+            //var erstellen die es ermöglicht dass man nur einmal in die methode rein geht, solange EndEpisode noch nicht ausgeführt wurde
+            checkLoopEntryForEndEpisode = true;
+            Debug.Log("Drin");
+            //EndEpisode();
+
+
+
+            System.Timers.Timer aTimer = new System.Timers.Timer();
+            aTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            aTimer.Interval = 100;
+            aTimer.Enabled = true;
+;
+
+
+            //wenn ich das gegenwärtige problem bekomme dann 
+
+
+            //ich nehme mir datetime now und speicher in var
+            //erstelle if loop mit differenz zwischen var aus step 1 und current wenn > 2sek endepisode check = false
+            
+
+
+            //ich kann auch die zeit von der current date time nehmen und diese in einer var speichern. in einem loop dann immer die differenz zwsichen beiden zeiten abfragen und bei 2sec ednepisode
+            //start timer here and 
+            /*EndEpisode();
+            Task.Delay(new TimeSpan(0, 0, 2)).ContinueWith(o => { EndEpisode(); });
+            System.Threading.Tasks.Task.Delay(1000).ContinueWith( (_) => checkLoopEntryForEndEpisode = false );
+            System.Threading.Tasks.Task.Delay(1000).ContinueWith( (_) => EndEpisode() );
+            System.Threading.Tasks.Task.Delay(1000).ContinueWith( (_) => Debug.Log("HALLALALLLALLLA") );*/
+            countForEndEpisode = 0;
+            //wenn es war ist dass das goal von einem laser getroffen wird dann könnte ich beim ersten mal einen timer starten 
+            //in jeder runde von actions checke ich dann den satus von dem tmer und bei einer bestoimmten zeit endepisode und checkLoopEntryForEndEpisode = true
+        }
+        
     }
 
+    public void OnTimedEvent(object source, ElapsedEventArgs e)
+    {
+        //Debug.Log("DASASASASAS");
+        EndEpisode();
+        Debug.Log("DASASASASAS");
+    }
     //just for testing
     public override void Heuristic(in ActionBuffers actionsOut){
         ActionSegment<float> continuousActions = actionsOut.ContinuousActions;
--- LaserfoundOne/Assets/LaserfoundOne/Laser.cs
+++ LaserfoundOne/Assets/LaserfoundOne/Laser.cs
@@ -6,6 +6,8 @@ public class Laser : MonoBehaviour
 {
     private LineRenderer lr;
 
+    private bool checkForReach = false;
+
     //reference to my AgentScript
     AgentScript agentScript;
 
@@ -17,16 +19,9 @@ public class Laser : MonoBehaviour
 
     public Rigidbody goalRigidbody;
 
-//    rd = goal.GetComponent<RigidBody>();
-
     // Start is called before the first frame update
     void Start()
     {   
-
-        //goalRigidbody.AddForce(5, 5, 5);
-        //goal.GetComponent<Rigidbody>().AddForce(0, 0, 1);
-        //Debug.Log(goal.GetComponent<Rigidbody>());        
-        //before assigning data i wanna assign variables from other scripts
         agentScript = agent.GetComponent<AgentScript>();
         lr = GetComponent<LineRenderer>();
     }
@@ -34,75 +29,29 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log
-        //ich muss kinematic disablen weil ich sonst keine kraft auf mein goal ausüben kann, deswegen fliegt mein goal dann aber durch die Plattform
-        //nach einem thread aus unity forum muss ich einen capsule collider nutzen um trozdem addforce hinzufügen zu können
-
-        //ich muss kinematic bei meinem goal an lassen, weil er sonst durch die plattform fliegt, daher kann ich kein gravity basierendes addforce benutzen
-        //ich muss etwas anderes finden für die bewegung
-
-        //1. funktion von laser mit radius
-        //2. funktion von laser mit radius und gravity
-        //3. git push 
-
-        //kann ich nicht einfach immer direkt disablen und enablen based on variable
+        //lila ray gets enabled 
         if(agentScript.checkIfRayHitsObject == true){
             this.GetComponent<LineRenderer>().enabled = true;
+            checkForReach = true;
         }
-        else this.GetComponent<LineRenderer>().enabled = false;
-        //Debug.Log(agentScript.checkIfRayHitsObject);
+        else {
+            this.GetComponent<LineRenderer>().enabled = false;
+            checkForReach = false;
+        }
 
         lr.SetPosition(0, transform.parent.position);
         RaycastHit hit; 
         if(Physics.Raycast(transform.parent.position, transform.forward, out hit)){
             if(hit.collider){
-                //here das collision mit addforce bearbeiten
-                Debug.Log("tets");
-                goalRigidbody = GameObject.Find("Goal").GetComponent<Rigidbody>();
-                //ich kann https://docs.unity3d.com/ScriptReference/Transform-position.html transform.position verwenden, um mein objekt zu bewegen
-                //frage ist bloß ob ich die methode auf meinen rigidbody oder auf meinem gameobject aufrufen muss?
-                //ich kann mit einem log einfach testen ob ich von rigidbody oder dem gameobject eine position bekomme, da wo ich sie bekomme, dass ist richtig
-                //ich kann auf den rigidbody position zugreifen
-                goalRigidbody.AddForce(0,0,2,ForceMode.Impulse);
+                if(hit.collider.gameObject.name == "Goal" && checkForReach == true){
+                    goalRigidbody = GameObject.Find("Goal").GetComponent<Rigidbody>();
+                    goalRigidbody.AddForce(0,0,1,ForceMode.Impulse);
+                    agentScript.checkIfRayMovedGoal = true;
+                }
+                else agentScript.checkIfRayMovedGoal = false;
                 lr.SetPosition(1, hit.point);
             }
         }
         else lr.SetPosition(1, transform.forward*5000);
-    
-        /*if(agentScript.checkIfRayHitsObject == true){
-            //his.GetComponent<LineRenderer>().enabled = true;
-            lr.SetPosition(0, transform.parent.position);
-            RaycastHit hit; 
-            if(Physics.Raycast(transform.parent.position, transform.forward, out hit)){
-                if(hit.collider){
-                    //here das collision mit addforce bearbeiten
-                    lr.SetPosition(1, hit.point);
-                }
-            }
-            else lr.SetPosition(1, transform.forward*5000);
-        }*/
-        /*
-        //if goal gets hit from ray sensor make ray (linerenderer visible)
-        if(agentScript.checkIfRayHitsObject == true){
-
-            /* testing von laser vorrübergehend
-            goalRigidbody = GameObject.Find("Goal").GetComponent<Rigidbody>();
-            goalRigidbody.AddForce(0,0,2,ForceMode.Impulse);
-            this.GetComponent<LineRenderer>().enabled = true;
-            lr.SetPosition(1, transform.parent.position);
-            RaycastHit hit; 
-            if(Physics.Raycast(transform.parent.position, transform.forward, out hit)){
-                if(hit.collider){
-                    lr.SetPosition(1, hit.point);
-                }
-            }
-            //else lr.SetPosition(1, transform.forward*5000);
-            //else this.GetComponent<LineRenderer>().enabled = false;
-        }
-        else
-        {
-            //else disable ray
-            this.GetComponent<LineRenderer>().enabled = false;
-        }*/
     }
 }
